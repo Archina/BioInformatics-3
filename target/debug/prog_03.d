2f265619e53022227385226be48ada58b2ad7738
Archina Void <archina5thrider@gmail.com>
2020-06-15 22:24:58 +0200

    remove unnecessary payload

--- target/debug/prog_03.d
+++
@@ -1,1 +0,0 @@
-/home/runner/BioInformatics-U3/target/debug/prog_03: /home/runner/BioInformatics-U3/src/main.rs /home/runner/BioInformatics-U3/src/matrix.rs /home/runner/BioInformatics-U3/src/sequences.rs
